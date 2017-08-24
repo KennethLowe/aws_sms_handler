83787bc16d8ba889288fca111983add2f16c7ce4
Kenny Lowe <kennethrlowe@gmail.com>
2017-08-24 00:06:13 -0700

    Adding logging, parsing incoming sms

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-var http = require('http');
-var aws = require('aws-lib');
-var twilio  = require('twilio');
+"use strict";
+const http = require('http');
+const aws = require('aws-lib');
+const express = require('express');
+const config = require('./config');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const log = require('simple-node-logger').createSimpleFileLogger('/tmp/sms_handler.log');
+const bodyParser = require('body-parser')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
-}).listen(3001);
+log.setLevel('debug');
 
-console.log('Server running at http://localhost:3001/');
+
+function getProductRating(product_code) {
+  let url = `https://www.amazon.com/gp/customer-reviews/widgets/average-customer-review/popover/ref=dpx_acr_pop_?contextId=dpx&asin=${product_code}`;
+  axios.get(url).then((res) => {
+    let idx = res.data.indexOf(' out of 5 stars');
+    return = res.data.substring(idx-3, idx+15);
+  });
+}
+
+const app = express();
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/sms', (req, res) => {
+  log.info('Incoming message');
+  const twiml = new MessagingResponse();
+  let msg = '';
+  if (req.body !== undefined) {
+    msg = `${req.body.From} texted me ${req.body.Body}`;
+	const upc = req.body.Body;
+  } else {
+    msg = 'Could not parse message';
+  }
+  log.info(msg);
+  twiml.message(msg);
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3001, () => {
+  console.log("Express server listening on port 3001");
+});
+
